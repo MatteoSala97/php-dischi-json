731c5ef2fea7e461529bef081f91e4a380b1e749
Matteo Sala <matteo111997@gmail.com>
2024-02-28 20:11:30 +0100

    config

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"  data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Dischi</title>
+
+    <!-- Boostrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+    <!-- FontAwesome library import link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <!-- My CSS -->
+    <!-- <link rel="stylesheet" href="./style.css"> -->
+
 </head>
-<body>
-    
+<body class="vh-100">
+    <div id="app">
+        <div class="container-fluid">
+            
+        </div>
+    </div>
+
+
+    <!-- Bootstrap JS -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.min.js' integrity='sha512-WW8/jxkELe2CAiE4LvQfwm1rajOS8PHasCCx+knHG0gBHt8EXxS6T6tJRTGuDQVnluuAvMxWF4j8SNFDKceLFg==' crossorigin='anonymous'></script>
+
+    <!-- Axios CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+    <!-- VUE3 CDN -->
+    <script src="https://unpkg.com/vue@3"></script>
+
+    <!-- My JS -->
+    <script src="./main.js"></script>
 </body>
 </html>
---
+++ main.js
@@ -0,0 +1,17 @@
+const {
+    createApp
+} = Vue
+
+createApp({
+    data(){
+        return {
+            apiUrl:"server.php",
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+        // qui ci va la chiamata axios
+    }
+}).mount("#app")
